77f335bed3b2cfcacf346db1d3a8118d588ef4a3
Ajinkyaj21 <ajinkya.jagadale@flairminds.com>
2024-07-29 12:21:13 +0530

    removed unwanted files

--- RDBMSbackend/dataset.js
+++
@@ -1,9 +0,0 @@
-const faker = require('faker')
-
-faker.seed(123)
-
-const products =  [...Array(80)].map((_) => ({
-    productName : faker.commerce.productName(),
-}))
-
-module.exports= {products}
